d7271128f0494cae52583865d7a82d16db5a7a91
duytrantlu <duytrinhmi@gmail.com>
2020-06-18 16:30:22 +0700

    fix bug

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,54 +9,44 @@ const storeController = require('../api/controllers/storeController');
 const wooController = require('../api/controllers/wooHooks');
 const orderController = require('../api/controllers/orderController');
 
-// GET /api/messages/public1
-router.get('/messages/public1', messageController.getPublicMessage1);
-
-// GET /api/messages/private1
-router.get('/messages/private1', authCheck(), messageController.getPrivateMessage1);
-
-// GET /api/messages/admin1
-router.get('/messages/admin1', authCheck([Roles.admin,Roles.siteAdmin]), messageController.getAdminMessage1);
-
-
 // GET /api/users
 router.get('/users', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.list);
 
 // GET /api/users/:id
-router.get('/users/:id', authCheck([Roles.siteAdmin]), userController.find);
+router.get('/users/:id', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.find);
 
 // DELETE /api/users/:id
-router.delete('/users/:id', authCheck([Roles.siteAdmin]), userController.destroy);
+router.delete('/users/:id', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.destroy);
 
 // DELETE many /api/users/:id
-router.delete('/users/ids/:ids', authCheck(), userController.destroys);
+router.delete('/users/ids/:ids', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.destroys);
 
 // PUT /api/users
-router.put('/users', authCheck([Roles.siteAdmin]), userController.updateUser);
+router.put('/users', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.updateUser);
 
 // PUT /api/users/password
-router.put('/users/password', authCheck([Roles.siteAdmin]), userController.updatePassword);
+router.put('/users/password', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.updatePassword);
 
 // PUT /api/users/role
-router.put('/users/role', authCheck(), userController.updateRole);
+router.put('/users/role', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.updateRole);
 
 // PUT /api/users/profile
-router.put('/users/profile', authCheck(), userController.updateProfile);
+router.put('/users/profile', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.updateProfile);
 
 // PUT /api/users/profile/password
-router.put('/users/profile/password', authCheck(), userController.updateProfilePassword);
+router.put('/users/profile/password', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), userController.updateProfilePassword);
 
 // GET /api/stores
-router.get('/stores',authCheck(), storeController.list);
+router.get('/stores',authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), storeController.list);
 
 // POST /apu/stores
-router.post('/stores',authCheck(), storeController.create);
+router.post('/stores',authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), storeController.create);
 
 // PUT /api/store
-router.put('/stores', authCheck(), storeController.updateStore);
+router.put('/stores', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), storeController.updateStore);
 
 // DELETE many store /api/stores/:ids
-router.delete('/stores/:ids', authCheck(), storeController.destroys);
+router.delete('/stores/:ids', authCheck([Roles.admin,Roles.siteAdmin, Roles.user]), storeController.destroys);
 
 // new API for Hook
 router.post('/woo/hooks', wooController.test);
